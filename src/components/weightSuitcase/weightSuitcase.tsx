e280028569b7e2cda4ba2e77206505e4f745868b
Javierfg86 <Javierfg86@outlook.es>
2022-07-13 12:13:41 +0200

    Add page Register and connect with DB

--- src/components/weightSuitcase/weightSuitcase.tsx
+++ src/components/weightSuitcase/weightSuitcase.tsx
@@ -1,4 +1,23 @@
 export function WeightSuitcase() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    async function handleSubmit(ev: SyntheticEvent) {
+        ev.preventDefault();
+        const registerUser = await new SuitcasesRepository().registerUser(
+            formData
+        );
+        console.log(registerUser);
+    }
+
+    function handleChange(ev: SyntheticEvent) {
+        const element = ev.target as HTMLFormElement;
+        setFormData({ ...formData, [element.name]: element.value });
+    }
+
     return (
         <div className="WeightSuitcase">
             <form>
